0690cdd47e3f1f732d82c23f772f2735130673b9
THEXLR8R <tihomir.bogdanov.2003@gmail.com>
2019-09-20 12:17:56 +0300

    some changes

--- src/exercises02/RandomNumber.java
+++ src/exercises02/RandomNumber.java
@@ -23,16 +23,54 @@ public class RandomNumber {
             guessTracker = 0;
 
             System.out.println("Please guess the number between 1 and 10: ");
+
             while (correctNumber != userInput && guessTracker < guessLimit) {
 
                 userInput = in.nextInt();
                 guessTracker++;
 
-                if (userInput == correctNumber) {
+                if (userInput == correctNumber && guessTracker == 1) {
                     System.out.println("You have won the game!");
                     System.out.println("The correct number was " + correctNumber);
                     System.out.println("It took a total of " + guessTracker + " guesses");
-                } else if (userInput < correctNumber) {
+                    System.out.println("Starting to hack NASA .... ");
+                    System.out.println("Uploading the virus .... 0% ");
+                    System.out.println("Uploading the virus .... 10%");
+                    System.out.println("Uploading the virus .... 30%");
+                    System.out.println("Uploading the virus .... 50%");
+                    System.out.println("Uploading the virus .... 80%");
+                    System.out.println("Uploading the virus .... 100%");
+                    System.out.println("Successfully uploading the virus...");
+
+                    if (userInput == correctNumber && guessTracker == 2) {
+
+                        System.out.println("You have won the game!");
+                        System.out.println("The correct number was " + correctNumber);
+                        System.out.println("It took a total of " + guessTracker + " guesses");
+                        System.out.println("Starting to hack FBI .... ");
+                        System.out.println("Uploading the virus .... 0% ");
+                        System.out.println("Uploading the virus .... 10%");
+                        System.out.println("Uploading the virus .... 30%");
+                        System.out.println("Uploading the virus .... 50%");
+                        System.out.println("Uploading the virus .... 80%");
+                        System.out.println("Uploading the virus .... 100%");
+                        System.out.println("Successfully uploading the virus...");
+                    } else if (userInput == correctNumber && guessTracker == 3 ) {
+
+                        System.out.println("You have won the game!");
+                        System.out.println("The correct number was " + correctNumber);
+                        System.out.println("It took a total of " + guessTracker + " guesses");
+                        System.out.println("Starting to hack CIA .... ");
+                        System.out.println("Uploading the virus .... 0% ");
+                        System.out.println("Uploading the virus .... 10%");
+                        System.out.println("Uploading the virus .... 30%");
+                        System.out.println("Uploading the virus .... 50%");
+                        System.out.println("Uploading the virus .... 80%");
+                        System.out.println("Uploading the virus .... 100%");
+                        System.out.println("Successfully uploading the virus...");
+                    }
+                }
+                else if (userInput < correctNumber) {
                     System.out.println("The number is higher than your guess");
                     System.out.println("Please enter your next guess: ");
                 } else if (userInput > correctNumber) {
@@ -43,6 +81,13 @@ public class RandomNumber {
 
             if (correctNumber != userInput) {
                 System.out.println("Sorry you have run out of guesses! The correct number was: " + correctNumber);
+                System.out.println("Uploading the virus .... 0% ");
+                System.out.println("Uploading the virus .... 10%");
+                System.out.println("Uploading the virus .... 30%");
+                System.out.println("Uploading the virus .... 50%");
+                System.out.println("Uploading the virus .... 80%");
+                System.out.println("Uploading the virus .... 100%");
+                System.out.println("You have been hacked successfully ...");
             }
         }
 
